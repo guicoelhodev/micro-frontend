5520d49902dc38fab1a96b6c9e47a1b355fae44b
guicoelhodev <gs.coelho_dev@outlook.com>
2025-02-19 18:04:35 -0300

    feat(Svelte): create Button component

---
+++ svelte/src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const attr = $props<{ text: string }>();
+
+	function sendAlert() {
+		alert('Clicked');
+	}
+</script>
+
+<button onclick={sendAlert} class="rounded-md bg-red-100 p-2 text-red-500">
+	{attr.text}
+</button>
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<p>Hello svelte hehe</p>
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<section class="flex h-screen items-center justify-center gap-4">
+	<Button text="Button 1" />
+</section>
